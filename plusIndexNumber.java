371fd412bf9b0618da53b6daa7f83c30c03e9a86
박민영 <102454078+chunngye@users.noreply.github.com>
2023-01-17 23:30:20 +0900

    자릿수 더하기

---
+++ plusIndexNumber.java
@@ -0,0 +1,31 @@
+public class plusIndexNumber {
+    public static void main(String[] args) {
+/*
+        // 풀이 1
+        public int solution_1(int n) {
+            int answer = 0;
+            String[] array = String.valueOf(n).split("");
+            for(String s : array){
+                answer += Integer.parseInt(s);
+            }
+            return answer;
+        }
+
+
+        //풀이 2
+        public int solution_2(int n){
+            int answer = 0;
+            String str = Integer.toString(n);
+            for (int i = 0; i < str.length(); i++) {
+                answer += Integer.parseInt("" + str.charAt(i));
+            }
+            return answer;
+        }
+
+        //풀이 3
+        String str = Integer.toString(n);
+        for (int i = 0; i < str.length(); i++) {
+            answer += Integer.parseInt("" + str.charAt(i));
+        }*/
+    }
+}
